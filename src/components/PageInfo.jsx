63a4d075b7d0b5e4e6c2f843071b74318b99cd65
Riccardo Tempesta <riccardo.tempesta@gmail.com>
2024-10-02 16:08:14 +0200

    Link to open customer in admin

--- src/components/PageInfo.jsx
+++ src/components/PageInfo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const PageInfo = ({ debugInfo }) => {
-    if (!debugInfo.settings) {
+    if (!debugInfo?.settings) {
         return null;
     }
 
@@ -12,13 +12,13 @@ const PageInfo = ({ debugInfo }) => {
             <AccordionItem key="page-info" aria-label="Page Information" title="Page Information" classNames={{title: "text-sm font-semibold"}}>
                 <div className={"grid grid-cols-[120px_1fr] pb-2 pl-4 items-center"}>
                     <div className="text-xs w-16">Store Hash</div>
-                    <div><span className="font-semibold text-xs">{ debugInfo.settings?.store_hash }</span></div>
+                    <div><span className="font-semibold text-xs">{ debugInfo.settings.store_hash }</span></div>
                     
                     <div className="text-xs">Store URL</div>
-                    <div><span className="font-semibold text-xs">{ debugInfo.settings?.base_url }</span></div>
+                    <div><span className="font-semibold text-xs">{ debugInfo.settings.base_url }</span></div>
 
                     <div className="text-xs">Page Title</div>
-                    <div><span className="font-semibold text-xs">{ debugInfo.head?.title }</span></div>
+                    <div><span className="font-semibold text-xs">{ debugInfo.head.title }</span></div>
                     
                     <div className="text-xs">Page Type</div>
                     <div><span className="font-semibold text-xs">{ debugInfo.page_type }</span></div>
@@ -32,7 +32,7 @@ const PageInfo = ({ debugInfo }) => {
                     <AccordionItem key="user-info" aria-label="User Information" title="User Information" classNames={{title: "text-sm font-semibold"}}>
                         <div className={"grid grid-cols-[120px_1fr] pb-2 pl-4 items-center"}>
                             <div className="text-xs">Customer ID</div>
-                            <div><span className="font-semibold text-xs">{ debugInfo.customer.id }</span></div>
+                            <div><span className="font-semibold text-xs">{ debugInfo.customer.id }</span> [<a target="_blank" className="text-green-500" href={`https://store-${debugInfo.settings.store_hash}.mybigcommerce.com/manage/customers/${debugInfo.customer.id}/edit`}>open in admin</a>]</div>
 
                             <div className="text-xs">Customer Name</div>
                             <div><span className="font-semibold text-xs">{ debugInfo.customer.name }</span></div>
